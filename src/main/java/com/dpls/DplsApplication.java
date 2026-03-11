b40f9b234f9ec6193fdd81f7514d03117a3dc633
Your Name <you@example.com>
2026-03-11 13:06:57 +0300

    corsconfig

--- src/main/java/com/dpls/DplsApplication.java
+++ src/main/java/com/dpls/DplsApplication.java
@@ -2,32 +2,11 @@ package com.dpls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @SpringBootApplication
 @EnableScheduling
 public class DplsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DplsApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**")
-						.allowedOriginPatterns("*")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-						.allowedHeaders("*")
-						.exposedHeaders("Authorization")
-						.allowCredentials(false)
-						.maxAge(3600);
-			}
-		};
 	}
 }
